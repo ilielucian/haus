afce476249c044e3eabc7dc71c603ca46e5cd33e
ilie-vlad <vlad.iulian.ilie@gmail.com>
2018-08-11 15:19:04 +0300

    Cleanup project: - removed grid component (using rental-card instead)

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { GridElement } from './directives/grid.element.component';
 import { RentalCardComponent } from './dashboard/rental-card/rental-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridElement,
     RentalCardComponent
   ],
   imports: [
